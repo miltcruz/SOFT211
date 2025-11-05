fb4d57ea4b3fcb28c5a4d847967f93b5151199fe
Milton Cruz <mr.cruzm@gmail.com>
2025-11-05 10:53:57 -0800

    Added local storage

--- app/(tabs)/tab.tsx
+++ app/(tabs)/tab.tsx
@@ -1,26 +1,48 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function Tab() {
 
-
-    const [count, setCount] = useState(0);
-    const [message, setMessage] = useState('Ready');
+    const [data, setData] = useState(null);
+    const [message, setMessage] = useState('');
 
 
     useEffect(() => {
-        setMessage(count % 2 === 0 ? 'Even count' : 'Odd count');
-    }, [count]);
+        const fetchData = async () => {
+            const cachedData = await AsyncStorage.getItem('todo');
+
+            if (cachedData) {
+                setData(JSON.parse(cachedData));
+                setMessage('Loaded data from cache.');
+            }
+            else {
+                fetch('https://jsonplaceholder.typicode.com/todos/1')
+                    .then(response => response.json())
+                    .then(json => {
+                        setData(json);
+                        AsyncStorage.setItem('todo', JSON.stringify(json));
+                        setMessage(`Fetched todo: ${json.title}`);
+                    })
+                    .catch(error => {
+                        setMessage(`Error fetching data: ${error.message}`);
+                    });
+            }
+        };
+
+        fetchData();
+    }, []);
 
 
 
     return (
         <ThemedView style={styles.container}>
             <ThemedText style={styles.title}>Hello React Native!</ThemedText>
-            <ThemedText>{count}</ThemedText>
-            <Button title="Increment" onPress={() => setCount(c => c + 1)} />
+            <ThemedText>
+                data: {data ? JSON.stringify(data) : 'Loading...'}
+            </ThemedText>
             <ThemedText>{message}</ThemedText>
         </ThemedView>
     );
